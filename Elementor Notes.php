a125e2af1647f274422336f38c7d88ca69a8442a
MD.Ridwan <109476737+ridwan-shakil@users.noreply.github.com>
2023-11-14 21:20:38 +0600

    Update Elementor Notes.php

--- Elementor Notes.php
+++ Elementor Notes.php
@@ -1,3 +1,14 @@
 <?php
 
-A page that is using a template , Can't be edited by Elementor
+A page that is using a template, Can't be edited by Elementor
+
+// =======================================
+ // html design to elementor widget
+// =======================================
+
+Make a basic plugin for the Elementor widget
+Enquire JS, CSS, and bootstrap (try not to use bootstrap )
+Pest HTML code into  ' Elementor widget render function '
+If it has jquery pest  that too after HTML
+-------------------------------------------
+Add controls for the HTML code
